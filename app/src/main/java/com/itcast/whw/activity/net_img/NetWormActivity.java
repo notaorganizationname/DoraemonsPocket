9463adbd8e5c82a40b1bab5c90c67c6e396b4786
zack <sockwz@163.com>
2019-05-14 19:32:58 +0800

    这是zack的第一次提交

---
+++ app/src/main/java/com/itcast/whw/activity/net_img/NetWormActivity.java
@@ -0,0 +1,13 @@
+package com.itcast.whw.activity.net_img;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetWormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net_worm);
+    }
+}
